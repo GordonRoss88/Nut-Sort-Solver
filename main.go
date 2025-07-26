eead70cbce4b754c082b96b4409798825338fbc5
Paul <paulbanwell@beyondaerospace.com>
2025-07-26 15:42:03 -0700

    init

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package Nut_Sort_Solver
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func loadFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	buffer, err := io.ReadAll(file)
+	inputString := strings.ReplaceAll(string(buffer), "\n", "")
+	inputString = strings.ReplaceAll(inputString, " ", "")
+
+}
