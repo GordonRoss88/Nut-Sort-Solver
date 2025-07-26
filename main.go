9d1621dfbcff2b79531f64c0cb7bebe810761ec7
Paul <paulbanwell@beyondaerospace.com>
2025-07-26 16:05:29 -0700

    made state importer

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package Nut_Sort_Solver
 
 import (
-	"fmt"
 	"io"
 	"log"
+	"nuts/hello"
 	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	initialState := loadFile("test.nuts")
+	log.Println(string(initialState))
+	hello.Hello()
 }
 
-func loadFile(path string) {
+func loadFile(path string) []byte {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -26,5 +28,8 @@ func loadFile(path string) {
 	buffer, err := io.ReadAll(file)
 	inputString := strings.ReplaceAll(string(buffer), "\n", "")
 	inputString = strings.ReplaceAll(inputString, " ", "")
-
+	state := []byte(inputString)
+	twoBolts := [8]byte{0, 0, 0, 0, 0, 0, 0, 0}
+	state = append(state, twoBolts[:]...)
+	return state
 }
